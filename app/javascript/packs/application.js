41c77947eef37aae541cef5f3d933c4f264dd4a3
naoya7076 <naoya7076@gmail.com>
2021-03-31 09:06:24 +0900

    bootstrapの導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,6 +6,8 @@
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
+import "bootstrap"
+import "bootstrap/scss/bootstrap.scss"
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
