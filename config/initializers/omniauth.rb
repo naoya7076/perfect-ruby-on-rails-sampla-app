ab5b0b869237445885b20bd8c484ee4524f5a806
naoya7076 <naoya7076@gmail.com>
2021-04-05 21:38:13 +0900

    fix omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
-Rails.application.config.middleware.use Omniauth::Builder do
+Rails.application.config.middleware.use OmniAuth::Builder do
   if Rails.env.development? || Rails.env.test?
-    provider: github, "113e886227d307cc295f", "46445ba3e922b37520f452f728bb7405fb569cb7"
+    provider :github, "113e886227d307cc295f", "46445ba3e922b37520f452f728bb7405fb569cb7"
   else
     provider :github,
       Rails.application.credentials.github[:client_id],
