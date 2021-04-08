d07ac814656976ee129ce7d17af39edf4998b202
naoya7076 <naoya7076@gmail.com>
2021-04-09 00:07:26 +0900

    ログインとイベント周りの実装

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,18 @@
 class Event < ApplicationRecord
+  validates :name, presence: true, length: {maximum: 50}
+  validates :place, presence: true, length: {maximum: 100}
+  validates :content, presence: true, length: {maximum: 200}
+  validates :start_at, presence: true
+  validates :end_at, presence: true
+  validate :start_at_shoud_be_before_end_at
+
+  private
+
+  def start_at_shoud_be_before_end_at
+    return unless start_at && end_at
+    
+    if start_at >= end_at
+      errors.add(:start_at, "は終了時間よりも前に設定してください") 
+    end
+  end
 end
