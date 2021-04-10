c6fa3f3e5e87e9c0a9a43a667c9a819271ca365b
naoya7076 <naoya7076@gmail.com>
2021-04-10 20:25:11 +0900

    retirementsのviewの処理

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  resources :events do
-    resources :tickets
-  end
   root 'welcome#index'
   get "auth/:provider/callback" => "sessions#create"
   delete "/logout" => "sessions#destroy"
+
+  resources :retirements
+
+  resources :events do
+    resources :tickets
+  end
 end
