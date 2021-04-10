dd8e255d98284de9cac40941f0d0b9feff0c5f3c
naoya7076 <naoya7076@gmail.com>
2021-04-10 16:55:12 +0900

    編集と更新の追加

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -16,6 +16,17 @@ class EventsController < ApplicationController
     @event = Event.find(params[:id])
   end
 
+  def edit
+    @event = current_user.created_events.find(params[:id])
+  end
+
+  def update
+    @event = current_user.created_events.find(params[:id])
+    if @event.update(event_params)
+      redirect_to @event, notice: "更新しました"
+    end
+  end
+
   private
 
   def event_params
