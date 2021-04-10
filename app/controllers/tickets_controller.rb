5e09504a2296a41188c61e3bc4aa73c429b468cf
naoya7076 <naoya7076@gmail.com>
2021-04-10 19:17:18 +0900

    ticket追加のメソッドを作成

---
+++ app/controllers/tickets_controller.rb
@@ -0,0 +1,16 @@
+class TicketsController < ApplicationController
+  def new
+    raise ActionController::RoutingError, "ログイン状態でTicketsController#newにアクセス"
+  end
+
+  def createe
+    event = Event.find(params[:event_id])
+    @ticket = current_user.tickets.build do |t|
+      t.event = event
+      t.comment = params[:ticket][:comment]
+    end
+    if @ticket.save
+      redirect_to event, notice: "このイベントに参加表明しました"
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :events
+  resources :events do
+    resources :tickets
+  end
   root 'welcome#index'
   get "auth/:provider/callback" => "sessions#create"
   delete "/logout" => "sessions#destroy"
