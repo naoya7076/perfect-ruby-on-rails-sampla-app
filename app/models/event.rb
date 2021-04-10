8ddc4da5cc4ea80bbc0dd0f303dd9470cab8c61f
naoya7076 <naoya7076@gmail.com>
2021-04-10 19:58:32 +0900

    削除機能の実装

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
-  has_many :tickets
+  has_many :tickets, dependent: :destroy
   belongs_to :owner, class_name: "User"
   validates :name, presence: true, length: {maximum: 50}
   validates :place, presence: true, length: {maximum: 100}
